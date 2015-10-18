4c93127f7c36abb557765e51538919eeaa5471f1
Antoine Bolvy <antoine.bolvy@gmail.com>
2015-10-18 23:27:36 +0200

    Update dependencies. Update seeding script. Add /events route

--- bin/insert-event.js
+++ bin/insert-event.js
@@ -2,10 +2,34 @@
 'use strict';
 
 /**
+ * Transform an array of hash to some hash, using specified key as identifier.
+ * toHash([{id:1, a: "lol"}], "id") == {1: {id:1, a: "lol"}}
+ *
+ * Linearize key can remove the hash structure entirely, using the specified key as a value.
+ * toHash([{id:1, a: "lol"}], "id", "a") == {1: "lol"}
+ */
+var toHash = function(arr, prop, linearizeKey) {
+    var hash = {};
+
+    arr.forEach(function(e) {
+        var key = e[prop].toString();
+        if(linearizeKey) {
+            e = e[linearizeKey];
+        }
+
+        hash[key] = e;
+    });
+
+    return hash;
+};
+
+
+/**
  * Insert events to mongoDB from JSON
  */
 var mongoose = require('mongoose');
 var async = require('async');
+var rarity = require('rarity');
 
 var config = require("../config");
 mongoose.connect(config.mongoUrl);
@@ -19,24 +43,51 @@ if(!process.argv[2]) {
 }
 var events = require(process.cwd() + '/' + process.argv[2]);
 
-async.each(events, function create(item, cb) {
-  var newEvent = parseIntraEvent(item);
-  if(!newEvent) {
-    return cb();
-  }
+console.time('test');
 
-  Event.update({
-    intraId: newEvent.intraId
-  }, newEvent, {
-    upsert: true
-  }, function(err) {
-    console.log(arguments);
-    cb(err);
-  });
-}, function(err) {
-  if(err) {
-    throw err;
+async.waterfall([
+  function(cb) {
+    events = events.map(function(item) {
+      return parseIntraEvent(item);
+    });
+    events = events.filter(function(item) {
+      return item;
+    });
+    var ids = events.map(function(item) {
+      return item.intraId;
+    });
+    Event.find({
+      intraId: {
+        $in: ids
+      }
+    }, rarity.carry([events], cb));
+  },
+  function(events, eventsDb, cb) {
+    var hashedEvents = toHash(events, 'intraId');
+    var hashedDbEvents = toHash(eventsDb, 'intraId');
+    async.parallel([
+      function(cb) {
+        async.each(eventsDb, function create(item, cb) {
+          item.update(hashedEvents[item.id], cb);
+        }, cb);
+      },
+      function(cb) {
+        events = events.filter(function(item) {
+          return !hashedDbEvents[item.intraId];
+        });
+        if (events.length) {
+          Event.collection.insert(events, cb);
+        }
+        else {
+          cb();
+        }
+      }
+    ], cb);
   }
-  console.log("Done");
+], function(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.timeEnd('test');
   process.exit(0);
 });
--- config/routes.js
+++ config/routes.js
@@ -8,7 +8,10 @@ var middlewares = lib.middlewares;
 
 module.exports = function(app) {
   app.get('/', middlewares.isAuth, routes.index.get);
+
   app.get('/login', routes.login.get);
   app.post('/login', routes.login.post);
   app.get('/logout', middlewares.isAuth, routes.logout.get);
+
+  app.get('/events', middlewares.isAuth, routes.events.get);
 };
